dc21fd41c97891c7c6343c985e6bd89707e2ebe5
submarinoff <rsmarinoff@gmail.com>
2017-03-15 20:05:53 +0200

    File uploads through Postman. File doesn't upload through javascript

--- LiquidSoundBack/src/main/java/com/rsmarinoff/liquidsound/song/SongController.java
+++ LiquidSoundBack/src/main/java/com/rsmarinoff/liquidsound/song/SongController.java
@@ -7,7 +7,6 @@ package com.rsmarinoff.liquidsound.song;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.util.Iterator;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +15,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 /**
  *
@@ -34,18 +33,14 @@ public class SongController {
     @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = "application/json")
     public @ResponseBody
     Song uploadFileHandler(
-            MultipartHttpServletRequest request, HttpServletResponse response) throws IOException {
+            @RequestPart(name = "file", required = true) MultipartFile file, HttpServletResponse response) throws IOException {
 
         Song song = new Song();
-        Iterator<String> iterator = request.getFileNames();
-        MultipartFile file;
-        while (iterator.hasNext()) {
-            file = request.getFile(iterator.next());
-            song.setContentType(file.getContentType());
-            song.setName(file.getOriginalFilename());
-            song.setContent(file.getBytes());
-        }
 
+        String name = file.getOriginalFilename();
+        song.setContentType(file.getContentType());
+        song.setName(name);
+        song.setContent(file.getBytes());
         return songRepository.save(song);
     }
 
